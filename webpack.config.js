0dc00fe0d3b1aec4b915a1b1b96c21e9216418eb
sergkryk <sergkryk@ya.ru>
2021-10-19 20:27:27 +0300

    installs webpack and creates base config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Start Template',
+      template: path.resolve(__dirname, 'src/index.html'),
+    })
+    ],
+};
